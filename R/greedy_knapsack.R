f031dcf53b80e8734d7f183b68a63c1fae904089
AlanCT94 <caciqueta94@gmail.com>
2022-10-07 11:34:54 +0200

    roxygen

--- R/greedy_knapsack.R
+++ R/greedy_knapsack.R
@@ -1,12 +1,39 @@
-# RNGversion(min(as.character(getRversion()),"3.5.3"))
-#
-# set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
-# n <- 100000
-# knapsack_objects <-
-#   data.frame(
-#     w=sample(1:4000, size = n, replace = TRUE),
-#     v=runif(n = n, 0, 10000)
-#   )
+
+RNGversion(min(as.character(getRversion()),"3.5.3"))
+
+set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
+n <- 100000
+knapsack_objects <-
+  data.frame(
+    w=sample(1:4000, size = n, replace = TRUE),
+    v=runif(n = n, 0, 10000)
+  )
+
+
+
+
+#' Knapsack greedy approximation
+#'
+#' @param x is the data frame with the items value and weigth knapsack objects
+#' @param W is the weight limit of the knap sack
+#' @param type it runs the normal or plus(s+1) type or greedy
+#'
+#' @return List with values and elements
+#' @export
+#'
+#' @examples
+#' RNGversion(min(as.character(getRversion()),"3.5.3"))
+#' set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
+#' n <- 100000
+#' knapsack_objects <-
+#' data.frame(
+#'    w=sample(1:4000, size = n, replace = TRUE),
+#'    v=runif(n = n, 0, 10000)
+#'  )
+#' x <- knapsack_objects[1:8,]
+#' greedy_knapsack(x = x, W = 3500)
+#' greedy_knapsack(x = x, W = 3500, type = "plus")
+
 
 greedy_knapsack <- function(x,W, type= "normal"){
   stopifnot(class(x) == "data.frame") #check that is a data frame
